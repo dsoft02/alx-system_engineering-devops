efae556a3b7505ba898da38dcfd8bba859db6a17
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-05-09 00:12:22 -0700

    Task 0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import json
+import requests
+
+def get_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Softboi'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
