671824f891d6b6b24c69d1b45660d7528dd16107
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-05-09 00:24:32 -0700

    gac

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,9 +3,10 @@
 import json
 import requests
 
+
 def get_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Softboi'}
+    headers = {'User-Agent': 'Softboi/0.0.1'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import json
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Softboi/0.0.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    else:
+        print(None)
